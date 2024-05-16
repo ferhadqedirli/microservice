c8cf94815328c75714dd850d4c2375be8380d1eb
ferhadqedirli <ferhadqedirli38@gmail.com>
2024-05-17 01:27:07 +0400

    Refresh config at runtime using Spring Cloud Bus & Spring Cloud Config monitor

--- section6/v2-spring-cloud-config/accounts/src/main/java/com/eazybytes/accounts/dto/AccountContactInfoDto.java
+++ section6/v2-spring-cloud-config/accounts/src/main/java/com/eazybytes/accounts/dto/AccountContactInfoDto.java
@@ -1,12 +1,14 @@
 package com.eazybytes.accounts.dto;
 
 import lombok.AccessLevel;
+import lombok.Data;
 import lombok.experimental.FieldDefaults;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
+@Data
 @ConfigurationProperties(prefix = "accounts")
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class AccountContactInfoDto {
--- section6/v2-spring-cloud-config/cards/src/main/java/com/eazybytes/cards/dto/CardContactInfoDto.java
+++ section6/v2-spring-cloud-config/cards/src/main/java/com/eazybytes/cards/dto/CardContactInfoDto.java
@@ -1,12 +1,14 @@
 package com.eazybytes.cards.dto;
 
 import lombok.AccessLevel;
+import lombok.Data;
 import lombok.experimental.FieldDefaults;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
+@Data
 @ConfigurationProperties(prefix = "cards")
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class CardContactInfoDto {
